6c1e8c39efccc614dce11caa12ba118e02317366
Tim Russell <tim@jatra.co.uk>
2019-06-22 15:13:07 +0100

    Ignore serverUrl asset

--- app/src/main/java/uk/co/jatra/noted/di/ApiModule.kt
+++ app/src/main/java/uk/co/jatra/noted/di/ApiModule.kt
@@ -3,16 +3,15 @@ package uk.co.jatra.noted.di
 import dagger.Module
 import dagger.Provides
 import uk.co.jatra.noted.network.Api
-import uk.co.jatra.noted.network.BASE_URL
 import uk.co.jatra.noted.network.RestAdapter
 import javax.inject.Inject
 import javax.inject.Named
 
 @Module
-class ApiModule @Inject constructor() {
+class ApiModule @Inject constructor(val baseUrl: String) {
     @Provides
     @Named("BaseUrl")
-    fun providesBaseUrl(): String = BASE_URL
+    fun providesBaseUrl(): String = baseUrl
 
     @Provides
     fun providesApi(adapter: RestAdapter): Api {
